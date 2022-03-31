8c924225526a10ff0e9d4c4dc37b1a96f77843f2
Ekaterina Sinelshchikova <katerinachistyakova93@yandex.ru>
2022-03-31 20:58:39 +0400

    добавоен бэм и js

--- README.md
+++ README.md
@@ -1,33 +1,16 @@
 # Проект: Место
-
-### Обзор
-
-* Figma
-* Картинки
-
-**Figma**
-
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
-
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
-# Проект: Путешествие по России 
-
 ## Описание
-Проектная работа по  адаптивной вёрстке 1280px, 1024px, 768px, 320px и работа с макетом в Figma
+Проектная работа по  адаптивной вёрстке и JS 1280px, 320px и работа с макетом в Figma
 
 ## Технологии
 
 - HTML
 - CSS
 - БЭМ
+- JS
 
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/5S2WSbEFL6awjVWJ0NWL8Q/Sprint-3_-Russia-_-desktop-mobile?node-id=28503%3A0)
+* [Ссылка на макет в Figma] (https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A10
 
 * [Ссылка на GitHub](https://github.com/EkaterinaSinelshchikova/russian-travel)
 
---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+let ProfileEditBtn = document.querySelector('.profile__edit-button');
+let Modalwindow = document.querySelector('.popup');
+let ModalCloseWindow = document.querySelector('.popup__close-button');
+
+let formElement = document.querySelector('.popup__content');
+let nameInput = formElement.querySelector('#name-input');
+let jobInput = formElement.querySelector('#job-input');
+
+const profileName = document.querySelector('.profile__title'); 
+const profileJob = document.querySelector('.profile__job');
+
+function openModalWindow () {
+    const name = profileName.textContent;
+    const job = profileJob.textContent;
+
+    nameInput.value = name;
+    jobInput.value = job;
+
+    Modalwindow.classList.add('popup__is-opened');
+}
+
+function closeModalWindow () {
+    Modalwindow.classList.remove('popup__is-opened');  
+}
+
+ProfileEditBtn.addEventListener('click',openModalWindow);
+
+ModalCloseWindow.addEventListener('click', closeModalWindow );
+
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+const name = nameInput.value;
+const job = jobInput.value;
+
+
+
+profileName.textContent = name;
+profileJob.textContent = job;
+
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
